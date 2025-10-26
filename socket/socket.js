0bb112fe9b301363c84fa7f5662f23f83a726568
Quankun1211 <117660970+Quankun1211@users.noreply.github.com>
2025-10-26 20:40:43 +0700

    ba

--- socket/socket.js
+++ socket/socket.js
@@ -1,36 +1,41 @@
 import { Server } from "socket.io";
-import http from "http"
-import express from 'express'
+import http from "http";
+import express from "express";
 
-const app = express()
+const app = express();
 
-const server = http.createServer(app)
+// Dùng chung allowedOrigins từ server.js
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://68fe22095ce7792a7e94ab2d--thriving-sundae-adaf5e.netlify.app",
+];
+
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST"]
-  }
-})
+    origin: allowedOrigins, // Dùng chung allowedOrigins
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 export const getReceiverSocketId = (receiverId) => {
-  return useSocketMap[receiverId]
-}
+  return useSocketMap[receiverId];
+};
 
-const useSocketMap = {} // {userId: socket}
+const useSocketMap = {};
 
-io.on('connection', (socket) => {
-  console.log(socket.id)
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  if (userId !== "undefined") useSocketMap[userId] = socket.id;
 
-  const userId = socket.handshake.query.userId
-  if(userId != "undefined") useSocketMap[userId] = socket.id
-
-  io.emit("getOnlineUsers", Object.keys(useSocketMap))
+  io.emit("getOnlineUsers", Object.keys(useSocketMap));
 
   socket.on("disconnect", () => {
-    console.log(socket.id);
-    delete useSocketMap[userId]
-    io.emit("getOnlineUsers", Object.keys(useSocketMap))
-  })
-})
+    console.log("Disconnected:", socket.id);
+    delete useSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(useSocketMap));
+  });
+});
 
-export {app, io, server}
+export { app, io, server };
