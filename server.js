b45377ed6d582e5e64529967d6afab9a6b8fd314
Quankun1211 <117660970+Quankun1211@users.noreply.github.com>
2025-10-26 22:30:38 +0700

    fg

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://68fe379b114e4e6ea20fdb58--golden-brigadeiros-c5bc0e.netlify.app",
   "https://stellular-mousse-67013d.netlify.app",
-  "https://imaginative-creponne-1f6f19.netlify.app",
-  "https://68fe3c73d22b697a431602ed--imaginative-creponne-1f6f19.netlify.app"
+  "https://ephemeral-froyo-cf835a.netlify.app",
+  "https://68fe3e646eae167f08e06411--ephemeral-froyo-cf835a.netlify.app"
 ];
 
 app.use(
