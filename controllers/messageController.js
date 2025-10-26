9f92c1c74fb4283a48ebad936645db403f208dad
Quankun1211 <117660970+Quankun1211@users.noreply.github.com>
2025-10-26 22:42:42 +0700

    send

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,49 +2,29 @@ import Conversation from "../models/conversationModel.js"
 import Message from "../models/messageModel.js"
 import { getReceiverSocketId, io } from "../socket/socket.js"
 
-export const sendMessage = async (req, res) => {
+const sendMessage = async (message) => {
+  setLoading(true);
   try {
-    const { message } = req.body
-    const { id: receiverId } = req.params
-    const senderId = req.user._id
-
-    let conversation = await Conversation.findOne({
-      participants: { $all: [senderId, receiverId]},
-    })
-
-    if(!conversation) {
-      conversation = await Conversation.create({
-        participants: [senderId, receiverId]
-      })
+    const res = await fetch(`https://backchat-5.onrender.com/api/messages/send/${selectedConversation._id}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ message }),
+      credentials: 'include'
+    });
+    const data = await res.json();
+    if (data.error) throw new Error(data.error);
+    if (data && typeof data === 'object' && data._id) {
+      setMessages([...messages, data]);
+    } else {
+      console.error('Invalid message data:', data);
     }
-
-    const newMessage = new Message({
-      senderId,
-      receiverId,
-      message
-    })
-
-    if(newMessage) {
-      conversation.messages.push(newMessage._id)
-    }
-
-    // await conversation.save()
-    // await newMessage.save()
-
-    await Promise.all([conversation.save(), newMessage.save()])
-
-    const receiverSocketId = getReceiverSocketId(receiverId)
-    if(receiverSocketId) {
-      io.to(receiverSocketId).emit("newMessage", newMessage)
-    }
-
-    res.status(201).json(newMessage)
-
   } catch (error) {
+    toast.error(error.message);
     console.log(error.message);
-    res.status(500).json({ error: "Internal server"})
+  } finally {
+    setLoading(false);
   }
-}
+};
 
 export const getMessage = async (req, res) => {
   try {
